e3730bb4d241dde43e1d2e200d8522ac685f54dd
theoneLee <623507020@qq.com>
2017-05-26 23:45:07 +0800

    完成： category模版和controller
    代办：
    其他模版和controller

--- src/main/java/News/controller/CategoryController.java
+++ src/main/java/News/controller/CategoryController.java
@@ -3,24 +3,26 @@ package News.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+
 /**
+ * 处理/category下的请求（比如请求某个分类下的全部新闻）
  * Created by Lee on 2017/5/26 0026.
  */
 @Controller
-public class IndexController {
+public class CategoryController {
 
-    @RequestMapping(value = "/")
-    public String index(Model model){
-        //从service拿到对应数据，然后封装到model
+    @RequestMapping(value = "/category")
+    public String getMoreCategory(@RequestParam(value = "name")String name,Model model){
         List<Category> categoryList=categoryService.getMoreCategory();//拿到更多分类，包含分类名和获取该分类下新闻的链接
         model.addAttribute("categoryList",categoryList);
-        List<News> newsList=newsService.getIndexNews();//拿到首页上最新的4条新闻，要包含标题，裁剪过的新闻内容，该新闻链接
-        model.addAttribute("news",newsList);
-        return "index";
-    }
 
+        model.addAttribute("page",newsService.getNewsByCategoryName(name));//使用spring data分页器接口
+
+        return "category";
+    }
 
 }
